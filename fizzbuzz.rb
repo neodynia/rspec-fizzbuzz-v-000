fdf67abb9818a8ccbd35b1de466770577dcb7a05
Ryan  Boris <ryan.boris@gmail.com>
2017-11-13 03:50:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int / 3
+  if int % 3 == 0
     return "Fizz"
-  elsif int / 5
+  elsif int % 5 == 0
     return "Buzz"
-  elsif int / 3 && int / 5
+  elsif int % 3 && int % 5
     return "FizzBuzz"
   else
   end
