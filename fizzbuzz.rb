f3491cfcebe4b9b7d07270fd2263b5bc32d1dbb5
Ryan  Boris <ryan.boris@gmail.com>
2017-11-13 03:52:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 3
     return "Fizz"
-  elsif int % 5 == 0
+  elsif int % 5 == 5
     return "Buzz"
-  elsif int % 3 && int % 5 == 0
+  elsif int % 3 && int % 5 == 15
     return "FizzBuzz"
   else
     return "nil"
